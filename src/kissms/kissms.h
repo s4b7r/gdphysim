d209a8d26f99029c11876f381ba64dcfdb038435
sbekemeier <devnull@localhost>
2014-01-30 22:14:11 +0100

    Add component includes to common include

--- src/kissms/kissms.h
+++ src/kissms/kissms.h
@@ -9,7 +9,29 @@
 #define KISSMS_H_
 
 
+// Include abstract component
+#include "kissms/component/component.h"
 
+// Include scalar leaf components
+#include "kissms/component/scalar-leaf/constant.h"
+#include "kissms/component/scalar-leaf/variable.h"
+
+// Include scalar components with one argument
+#include "kissms/component/scalar-one/cosinus.h"
+#include "kissms/component/scalar-one/negation.h"
+#include "kissms/component/scalar-one/reciprocal.h"
+#include "kissms/component/scalar-one/sinus.h"
+
+// Include scalar components with two arguments
+#include "kissms/component/scalar-two/addition.h"
+#include "kissms/component/scalar-two/multiplication.h"
+
+// Include vectorial components with one argument
+// TODO Does a vector belong to components with just one argument?
+#include "kissms/component/vector-one/vector.h"
+
+// Include vectorial components with two arguments
+#include "kissms/component/vector-two/vectorproduct.h"
 
 
 #endif /* KISSMS_H_ */
