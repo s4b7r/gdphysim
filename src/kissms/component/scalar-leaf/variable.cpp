c11e6b65c6a8edeb6eea9dc4ae37389821e8dbea
sbekemeier <devnull@localhost>
2014-02-01 19:15:03 +0100

    Add Variable method stubs

--- src/kissms/component/scalar-leaf/variable.cpp
+++ src/kissms/component/scalar-leaf/variable.cpp
@@ -9,6 +9,33 @@
 
 namespace kissms {
 
+Variable::Variable() {
+}
 
+Variable::~Variable() {
+}
+
+void Variable::setVariable(char* value) {
+}
+
+void Variable::setVariable(int value) {
+}
+
+void Variable::setVariable(double value) {
+}
+
+bool Variable::isCalculable() {
+
+	return false;
 
 }
+
+bool Variable::isQuantifiable() {
+
+	return false;
+
+}
+
+}
+
+
