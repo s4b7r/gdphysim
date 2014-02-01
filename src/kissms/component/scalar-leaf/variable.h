7405dc943ac313084e09fe87ec845feeb704efbe
sbekemeier <devnull@localhost>
2014-02-01 19:12:43 +0100

    Implement Variable

--- src/kissms/component/scalar-leaf/variable.h
+++ src/kissms/component/scalar-leaf/variable.h
@@ -11,9 +11,36 @@
 
 namespace kissms {
 
-class Variable {
+/**
+ * @brief Component representing a single variable value
+ */
+class Variable : public Component {
 
+public:
+	Variable();
+	virtual ~Variable();
 
+	/**
+	 * @brief Set a placeholder string as the Variable's value
+	 * @param value A zero-terminated character array representing the placeholder string
+	 */
+	void setVariable( char *value );
+
+	/**
+	 * @brief Set an integer value as the Variable's value
+	 * @param value The Variable's new integer value
+	 */
+	void setVariable( int value );
+
+	/**
+	 * @brief Set a double value as the Variable's value
+	 * @param value The Variable's new double value
+	 */
+	void setVariable( double value );
+
+	virtual bool isCalculable();
+
+	virtual bool isQuantifiable();
 
 };
 
