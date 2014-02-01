28e6d65f9fcfd3d74da99aa0dede10d0885d98f8
sbekemeier <devnull@localhost>
2014-02-01 14:14:21 +0100

    Add solve and calculate function declarations

--- src/kissms/equation.h
+++ src/kissms/equation.h
@@ -13,7 +13,33 @@ namespace kissms {
 
 class Equation : public ArgumentsTwo {
 
+public:
+	Equation(  );
+	virtual ~Equation(  );
 
+	/**
+	 * @brief Solves this Equation
+	 * @þaram variable Variable for which this Equation shall be solved
+	 * @return ResultCode
+	 *
+	 * The Equation is reformed so that the given Variable is explicitely represented
+	 * by the Equation's other side. The Variable's actual value won't be calculated.
+	 *
+	 * @see calculateFor()
+	 */
+	ResultCode solveFor( Variable *variable );
+
+	/**
+	 * @brief Solves and calculates this Equation
+	 * @param variable Variable for which this Equation shall be solved
+	 * @return ResultCode
+	 *
+	 * The Equation is solved using solveFor() and afterwards the Variable's value will
+	 * be calculated, as far as the Equation's other side allows it.
+	 *
+	 * @see solveFor()
+	 */
+	ResultCode calculateFor( Variable *variable );
 
 };
 
