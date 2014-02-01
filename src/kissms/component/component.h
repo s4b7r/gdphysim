f2b41089f5de12820455c4c4e8bc85eccb549826
sbekemeier <devnull@localhost>
2014-02-01 15:09:31 +0100

    Add isCalculable and isQuntifiable methods to Component

--- src/kissms/component/component.h
+++ src/kissms/component/component.h
@@ -23,6 +23,30 @@ public:
 	Component(  );
 	virtual ~Component(  );
 
+	/**
+	 * @brief Checks whether the Component is calculable
+	 * @retval True The Component contains no unresolved Variables
+	 * @retval False The Component contains at least one unresolved Variable
+	 *
+	 * Checks all child-Components for unresolved Variables. In case of finding at least one
+	 * unresolved Variable the Component is not calculable. Finding no unresolved Variables
+	 * does not proove the Component as representable by a numerical value.
+	 *
+	 * @see isQuantifiable()
+	 */
+	bool isCalculable(  );
+
+	/**
+	 * @brief Checks whether the Component is representable by a numerical value
+	 * @retval True The Component is representable by a numerical value
+	 * @retval False The Component is not representable by a numerical value
+	 *
+	 * Checks all child-Components whether they are representable by numerical values.
+	 *
+	 * @see isCalculable()
+	 */
+	bool isQuantifiable(  );
+
 };
 
 }
