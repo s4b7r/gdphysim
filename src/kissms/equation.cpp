61664d992ae2aaaf62c4d7be901ff08620f2fb5b
sbekemeier <devnull@localhost>
2014-02-01 15:14:40 +0100

    Add method stubs for Equation

--- src/kissms/equation.cpp
+++ src/kissms/equation.cpp
@@ -9,6 +9,46 @@
 
 namespace kissms {
 
+Equation::Equation() {
+}
 
+Equation::~Equation() {
+}
+
+ResultCode Equation::solveFor(Variable* variable) {
+
+	return NotYetImplemented;
+
+}
+
+ResultCode Equation::calculateFor(Variable* variable) {
+
+	return NotYetImplemented;
+
+}
+
+bool Equation::contains(Variable* variable) {
+
+	return false;
+
+}
+
+bool Equation::isOnLeft(Variable* variable) {
+
+	return false;
+
+}
+
+bool Equation::isOnRight(Variable* variable) {
+
+	return false;
+
+}
+
+bool Equation::isExplicitly(Variable* variable) {
+
+	return false;
+
+}
 
 }
