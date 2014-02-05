2e908ccb215a285311b942ce8408aee10d889fda
sbekemeier <devnull@localhost>
2014-02-05 20:45:26 +0100

    Create test main

---
+++ src/kissms/test/test.cpp
@@ -0,0 +1,17 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 05.02.2014
+ *      Author: sieb
+ */
+
+#include "kissms/kissms.h"
+
+int main(int argc, char **argv) {
+
+	kissms::Equation eq = new Equation();
+
+}
+
+
+
