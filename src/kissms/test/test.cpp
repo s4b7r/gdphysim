c45675e8cfad30e39655598b38c7fb89c20cc4bf
sbekemeier <devnull@localhost>
2014-02-05 21:55:17 +0100

    Finish first test

--- src/kissms/test/test.cpp
+++ src/kissms/test/test.cpp
@@ -5,11 +5,21 @@
  *      Author: sieb
  */
 
+#include <stdio.h>
 #include "kissms/kissms.h"
 
 int main(int argc, char **argv) {
 
 	kissms::Equation *eq = new kissms::Equation();
+	kissms::Variable *va = new kissms::Variable();
+	kissms::Constant *co = new kissms::Constant();
+	char *na = (char*) malloc(sizeof(char) * 2);
+	na[0] = 'a';
+	na[1] = 0;
+	eq->setArguments(va, co);
+	va->setName(na);
+	co->setValue(4);
+	eq->calculateFor(va);
 
 }
 
