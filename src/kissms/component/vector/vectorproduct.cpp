2969b6c2d6ce24a785229776b6da5f085e0f1d23
sbekemeier <devnull@localhost>
2014-02-11 13:05:31 +0100

    Implement Vectorproduct::getVector

--- src/kissms/component/vector/vectorproduct.cpp
+++ src/kissms/component/vector/vectorproduct.cpp
@@ -30,8 +30,27 @@ ResultCode Vectorproduct::calculate() {
 
 Vector* Vectorproduct::getVector() {
 
-	// TODO Vectorproduct::getVector
-	return 0;
+	Vector *result = new Vector();
+	Addition *add[3];
+	Multiplication *mu[3][2];
+	int index[2];
+	index[0] = 2;
+	index[1] = 3;
+
+	for( int i = 0; i < 3; i++ ) {
+		add[i] = new Addition();
+		for( int j = 0; j < 2; j++ ) {
+			mu[i][j] = new Multiplication();
+			mu[i][j]->setLeft(((Vector*)argumentLeft)->getArgument(index[j]));
+			mu[i][j]->setRight(((Vector*)argumentRight)->getArgument(index[1-j]));
+		}
+		add[i]->setArguments(mu[i][0], mu[i][1]);
+		result->setArgument(i, add[i]);
+		index[0] = index[0] == 3 ? 1 : index[0]+1;
+		index[1] = index[1] == 3 ? 1 : index[1]+1;
+	}
+
+	return result;
 
 }
 
